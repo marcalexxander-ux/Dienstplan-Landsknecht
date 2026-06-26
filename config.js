b593d22181b919698b0c0fa41b289918f1e495f0
marcalexxander-ux <marc.alexxander@gmail.com>
2026-06-26 18:47:49 +0200

    Alte config.js wiederhergestellt

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
-// Deine Supabase-Daten hier eintragen.
-// Wenn deine bestehende config.js schon funktioniert, diese Datei NICHT überschreiben.
+// Supabase Einstellungen hier eintragen.
+// Diese Werte bekommst du in Supabase unter: Project Settings → API
 
-window.SUPABASE_URL = "https://bqseyvnmampwwhfewhvv";
+window.SUPABASE_URL = "https://bqseyvnmampwwhfewhvv.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_t9qEQWqNnLkpdnGWbPsLKA_vylGKQui";
+
+
+
+
+
