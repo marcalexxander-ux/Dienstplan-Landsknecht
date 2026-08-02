1c5081850ca5e173b5a1e78d82638d1f489459c5
marcalexxander-ux <marc.alexxander@gmail.com>
2026-08-02 18:21:49 +0200

    v7.0.4 smartphone logo herunterladen

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let pendingStaffInvites=[];
 document.body.classList.add("loggedOut");
-const APP_VERSION="v7.0.3";
+const APP_VERSION="v7.0.4";
 const removedStaffIds=new Set();
 const MAX_EMPLOYEES=20;
 const days=["Mo","Di","Mi","Do","Fr","Sa","So"];
